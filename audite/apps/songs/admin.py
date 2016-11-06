f37c141aac820c7c23eb3da9ee7d1de096fb5a29
Ivan Ponce <ivanponce22@gmail.com>
2016-11-06 16:26:29 -0500

    Setting up the admin for songs app

--- audite/apps/songs/admin.py
+++ audite/apps/songs/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Artist)
+class ArtistAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "name_slug", "picture")
+    search_fields = ["name"]
+
+@admin.register(models.Album)
+class ALbumAdmin(admin.ModelAdmin):
+    list_display = ("artist", "name", "name_slug", "year", "genre")
+    search_fields = ["name", "year", "genre"]
+
+@admin.register(models.Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("album", "title", "name_slug", "duration", "calification")
+    search_fields = ["album", "title", "calification"]
